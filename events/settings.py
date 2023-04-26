2d72da52fb2a8675ff323d0cb6ad83f9d29e3cdc
EC2 Default User <ec2-user@ip-172-31-16-141.us-west-1.compute.internal>
2023-04-26 12:20:36 +0000

    Third Commit

--- events/settings.py
+++ events/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'events.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newdb_py',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'x22173536-cpp-project.c6vtzfrggjdy.us-west-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
